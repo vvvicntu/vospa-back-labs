111f55896e7b6ca700bb01b24104d60ca48568d4
Воспанчук Виктория <raleevaa@gmail.com>
2025-09-05 09:29:20 +0700

    Картинка

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__) 
 
 @app.route("/web")
@@ -26,3 +26,16 @@ def author():
             <a href="/web">web</a>
         </body>
     </html>"""
+
+@app.route("/image")
+def image():
+    path = url_for("static", filename="ledy bag.png")
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Леди Баг</h1>
+        <img src="''' + path + '''">
+    </body>
+</html>
+'''
