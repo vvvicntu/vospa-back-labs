5efcdf4300fa8a9a01a1666dbf7028d5cb2ca1b4
Воспанчук Виктория <raleevaa@gmail.com>
2025-09-05 09:07:57 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask
 app = Flask(__name__) 
 
-@app.route("/")
-def start():
+@app.route("/web")
+def web():
     return """<!doctype html>
         <html>
            <body>
                <h1>web-сервер на flask</h1>
+               <a href="/author">author</a>
            <body> 
         </html>""" 
 
+@app.route("/author")
+def author():
+    name = 'Воспанчук Виктория Влдаимировна'
+    group = 'ФБИ-31'
+    faculty = 'ФБ'
+
+    return """<!doctype html>
+    <html>
+        <body>
+            <p>Студент: """ + name + """ </p>
+            <p>Группа: """ + group + """ </p>
+            <p>Факультет: """ + faculty + """ </p>
+            <a href="/web">web</a>
+        </body>
+    </html>"""
