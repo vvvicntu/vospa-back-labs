bf2bb2d3ae144ed01b75b8e465866b6037bb46ee
Воспанчук Виктория <raleevaa@gmail.com>
2025-09-06 15:49:38 +0700

    Заголовки

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ def web():
                <h1>web-сервер на flask</h1>
                <a href="/author">author</a>
            <body> 
-        </html>""" 
+        </html>""", 200, {"X-Server": "sample",
+                          'Content-Type': 'text/plain; charset=utf-8'
+                          }
 
 @app.route("/author")
 def author():
