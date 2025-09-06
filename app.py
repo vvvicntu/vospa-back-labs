344a5c7729f509a26daa70509b2a58a9c17f7235
Воспанчук Виктория <raleevaa@gmail.com>
2025-09-06 15:10:13 +0700

    Ошибка 404

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, url_for, request, redirect
 from datetime import datetime
 app = Flask(__name__) 
 
+@app.errorhandler(404)
+def not_found(err):
+    return "Нет такой страницы :(", 404
+
 @app.route("/web")
 def web():
     return """<!doctype html>
@@ -77,3 +81,4 @@ def created():
     <body>
 </html>
 ''', 201
+
