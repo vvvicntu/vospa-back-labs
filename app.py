3922a1020fb620370954ceaa657a99f771cd3f0a
Воспанчук Виктория <raleevaa@gmail.com>
2025-09-06 14:47:51 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, redirect
 from datetime import datetime
 app = Flask(__name__) 
 
@@ -62,3 +62,6 @@ def counter():
     </body>
 </html>
 '''
+@app.route("/info")
+def info():
+    return redirect("/author")
