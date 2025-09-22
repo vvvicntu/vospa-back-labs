7fff99802f9722d0866168647b19b9bf0d52001d
Воспанчук Виктория <raleevaa@gmail.com>
2025-09-22 19:22:58 +0700

    Имя студента

--- app.py
+++ app.py
@@ -534,5 +534,6 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Воспанчук Виктория'
+    return render_template('example.html', name=name)
 
