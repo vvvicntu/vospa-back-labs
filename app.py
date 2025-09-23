e6b7f1cbdc6bd0ae4b70c06cbe44520c87a1668b
Воспанчук Виктория <raleevaa@gmail.com>
2025-09-23 14:55:31 +0700

    Фильтры

--- app.py
+++ app.py
@@ -552,3 +552,7 @@ def example():
 def lab2():
     return render_template('lab2.html')
 
+@app.route('/lab2/filters')
+def filters():
+    phrase = "О <b>сколько</b> <u>нам</u> <i>открытий</i> чудных..."
+    return render_template('filter.html', phrase=phrase)
