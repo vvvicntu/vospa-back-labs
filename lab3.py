d157256a9cf8cb3ad6682016341b826fd316a32c
Воспанчук Виктория <raleevaa@gmail.com>
2025-10-05 17:32:51 +0700

    Cookie

--- lab3.py
+++ lab3.py
@@ -1,7 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, make_response, redirect
 lab3 = Blueprint('lab3', __name__)
 
 
 @lab3.route('/lab3/')
 def lab():
-    return render_template('lab3.html')
+    name = request.cookies.get('name')
+    name_color = request.cookies.get('name_color')
+    return render_template('lab3.html', name=name, name_color=name_color)
+
+@lab3.route('/lab3/cookie')
+def cookie():
+    resp = make_response(redirect('/lab3/'))
+    resp.set_cookie('name', 'My God', max_age=5)
+    resp.set_cookie('age', '20')
+    resp.set_cookie('name_color', 'red')
+    return resp
+
+@lab3.route('/lab3/del_cookie')
+def del_cookie():
+    resp = make_response(redirect('/lab3/'))
+    resp.delete_cookie('name')
+    resp.delete_cookie('age')
+    resp.delete_cookie('name_color')
+    return resp
