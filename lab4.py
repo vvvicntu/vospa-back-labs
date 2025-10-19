121bd2210c8e1dc55829b7050d12fac956de0bec
Воспанчук Виктория <raleevaa@gmail.com>
2025-10-19 13:37:17 +0700

    валидация

--- lab4.py
+++ lab4.py
@@ -85,7 +85,7 @@ def step():
 
 tree_count = 0
 
-@lab4.route('/lab4/tree', methods = ['GET', 'POST'])
+@lab4.route('/lab4/tree', methods=['GET', 'POST'])
 def tree():
     global tree_count
 
@@ -94,9 +94,9 @@ def tree():
 
     operation = request.form.get('operation')
     
-    if operation == 'cut':
+    if operation == 'cut' and tree_count > 0:
         tree_count -= 1
-    elif operation == 'plant':
+    elif operation == 'plant' and tree_count < 10:
         tree_count += 1 
     
     return redirect('/lab4/tree')
