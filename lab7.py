309673e55c4d3240c6a5edf90f466056e892b71b
Воспанчук Виктория <raleevaa@gmail.com>
2025-11-26 17:19:56 +0700

    Редактирование выбранного фильма по REST

--- lab7.py
+++ lab7.py
@@ -74,3 +74,14 @@ def del_film(id):
         abort(404)
     del films[id]
     return '', 204
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
+def put_film(id):
+    if id < 0 or id >= len(films):
+        abort(404)
+    
+    film = request.get_json()
+    
+    films[id] = film
+    return films[id]
+
