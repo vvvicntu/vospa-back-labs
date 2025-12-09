08877d978a9262d994fb1d3464877c67cbb9bde5
Воспанчук Виктория <raleevaa@gmail.com>
2025-12-10 01:12:47 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, session, current_app
-
+from db import db
+from db.models import users, articles
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -7,3 +8,33 @@ lab8 = Blueprint('lab8', __name__)
 @lab8.route('/lab8/')
 def lab():
     return render_template('lab8/lab8.html', login=session.get('login'))
+
+@lab8.route('/lab8/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+    
+    # имя пользователя не должно быть пустым
+    if not login_form == '':
+        return render_template('lab8/register.html',
+                               error='Логин не может быть пустым')
+    
+    # пароль не должен быть пустым
+    if not password_form == '':
+        return render_template('lab8/register.html',
+                               error='Пароль не может быть пустым')
+    
+    login_exists = users.query.filter_by(login=login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html',
+                               error='Такой пользователь уже существует')
+    
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login=login_form, password=password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/lab8/')
+
