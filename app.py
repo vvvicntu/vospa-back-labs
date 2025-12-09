d8a18d0372e217b189133494fef0f8b9307b0e36
Воспанчук Виктория <raleevaa@gmail.com>
2025-12-09 23:36:08 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, url_for, request, redirect, abort, render_template
 from datetime import datetime
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -15,6 +19,24 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный ключ')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'vvv_orm'
+    db_user = 'vvv_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+
+else:
+    from os import path
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "ivan_ivanov_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
