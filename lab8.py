13bce98f59c228b59558b175bf1050a6f7df782b
Воспанчук Виктория <raleevaa@gmail.com>
2025-12-10 11:57:35 +0700

    Логаут

--- lab8.py
+++ lab8.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from db import db
 from db.models import users, articles
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, current_user, logout_user
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -70,3 +70,8 @@ def login():
                            error = 'Ошибка входа. Логин или пароль неверны')
 
 
+@lab8.route('/lab8/logout')
+@login_required # страница будет доступна только авторизованным пользователям
+def logout():
+    logout_user()
+    return redirect('/lab8/')
